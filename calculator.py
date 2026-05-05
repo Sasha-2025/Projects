fa48f14b1e45af05ae7cf78720342200ec1c3380
Sasha-2025 <sashasakr8@gmail.com>
2026-05-05 17:13:29 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def absolute_value(number):
+
+    if number >= 0:
+        return number
+    else:
+        return -number
+
+
+print("absolute value of 22 :", absolute_value(22))
+
+print("absolute value of -222 :", absolute_value(-222))
